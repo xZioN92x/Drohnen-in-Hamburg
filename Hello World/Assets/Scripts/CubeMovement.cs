bced5a35c8c0c02cda2d9a28a78be9cceeedfaf5
Aleks Desktop <aleksandar.petrusov@studium.uni-hamburg.de>
2022-04-29 12:36:51 +0200

    Movement Update

--- Hello World/Assets/Scripts/CubeMovement.cs
+++ Hello World/Assets/Scripts/CubeMovement.cs
@@ -13,21 +13,7 @@ public class CubeMovement : MonoBehaviour
 
     void Update()
     {
-        Vector3 movementVector = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.W)) 
-        {
-            movementVector = Vector3.forward;
-        } else if (Input.GetKey(KeyCode.S))
-        {
-            movementVector = Vector3.back;
-        } else if (Input.GetKey(KeyCode.A))
-        {
-            movementVector = Vector3.left;
-        } else if (Input.GetKey(KeyCode.D))
-        {
-            movementVector = Vector3.right;
-        }
+        Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
         transform.Translate(movementVector * Time.deltaTime * speed);
     }
