6fbb9bb588d79aeba0ae928d254cde4851f0abed
Aleks Desktop <aleksandar.petrusov@studium.uni-hamburg.de>
2022-05-02 18:06:38 +0200

    PC Update

---
+++ Drohnen-in-Hamburg-master/Hello World/Assets/Scripts/TestScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    public void Test()
+    {
+        Debug.Log("Hello!");
+        
+    }
+  
+}
